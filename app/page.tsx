cf2f4de62438c6d1c0337e17bbf889c8ab85f56c
Nikita Malienko <nekmalienko2001@gmail.com>
2024-07-23 20:53:11 +0300

    project init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Home Page</h1>
+            <Button variant="outline">Click me!</Button>
+        </>
+    );
+}
